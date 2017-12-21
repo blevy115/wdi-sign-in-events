08ab21e24f6b85e2f99027fb391fc180d8dc076a
Brandon Levy <brandon.levy@mail.mcgill.ca>
2017-12-21 14:53:52 -0500

    added error class when submit clicked and removed error when mouse hovered over element

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var signIn = document.body.querySelector('.signin')
-  var modal = document.body.querySelector('.modal')
+  var signIn = document.body.querySelector('.signin');
+  var modal = document.body.querySelector('.modal');
+  var close = modal.querySelector('.close');
+  var inputs = modal.querySelectorAll('input');
+  var submit = modal.querySelector('.submit')
   signIn.addEventListener('click', function(e){
     modal.style.display='block';
-  })
+  });
+  close.addEventListener('click', function(e){
+    modal.style.display='none';
+  });
+  submit.addEventListener('click', function(e){
+    inputs.forEach(function(input){
+      input.className = 'error';
+    });
+  });
+  inputs.forEach(function(input){
+    input.addEventListener('mouseenter', function(e){
+      e.target.classList.remove('error');
+    });
+  });
 });
